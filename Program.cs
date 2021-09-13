b3c54e458d9afb47b1a04107f5146aca0dc9cf91
Andrew <andrewmgunn26@gmail.com>
2021-09-13 14:10:14 -0500

    have total, mean, median calcuations so far

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Module5Lab
             string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
             int[] sightings = new int[5];
             int total = 0;
-            double avg = 0;
+            double mean = 0;
+            double median = 0;
 
             for (int i = 0; i < days.Length; i++)
             {
@@ -24,10 +25,12 @@ namespace Module5Lab
                 total += sightings[i];
             }
 
-            avg = total / sightings.Length;
+            mean = total / sightings.Length;
+            median = total / 2;
 
-            Console.WriteLine($"\nTotal:  {total}");
-            Console.WriteLine($"Average:  {avg:N1}");
+            Console.WriteLine($"\nTotal:  {total:F}");
+            Console.WriteLine($"Mean:  {mean:F}");
+            Console.WriteLine($"Median:  {median:F}");
 
         }
         
